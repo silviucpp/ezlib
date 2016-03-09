5f8e950289ebdce74162a6507f47f453bbf3e968
Silviu Caragea <silviu.caragea@woowteam.com>
2016-03-09 14:54:31 +0200

    Add concurent testing support for internal benchmark stanza.

--- testing/benchmark_stanza.erl
+++ testing/benchmark_stanza.erl
@@ -3,7 +3,7 @@
 
 -include("ezlib.hrl").
 
--export([run/6]).
+-export([run/6, multi_run/7]).
 
 ezlib_process(_Ref, _Lines, 0) ->
     ok;
@@ -46,6 +46,53 @@ run(ezlib, Path, Nr, Level, Bits, Mem) ->
     {Time, _} = timer:tc( fun() -> ezlib_process(DeflateRef, Lines, Nr) end),
     finish(Nr*length(Lines), Time),
     ezlib:metrics(DeflateRef).
+
+multi_run(ezlib, Path, NrProcesses, Nr, Level, Bits, Mem) ->
+    Lines = read_from_file(Path),
+    Options =
+        [
+            {compression_level, Level},
+            {window_bits, Bits},
+            {memory_level, Mem}
+        ],
+
+    List = create_ezlib_deflate_session(NrProcesses, Options),
+
+    io:format("creating deflate session completed ~n"),
+
+    {Time, _} = timer:tc(fun() -> plists:foreach(fun(DeflateRef) -> ezlib_process(DeflateRef, Lines, Nr) end, List) end),
+    finish(NrProcesses*Nr*length(Lines), Time),
+    [H|_] = List,
+    ezlib:metrics(H);
+
+multi_run(erlang, Path, NrProcesses, Nr, Level, Bits, Mem) ->
+    Lines = read_from_file(Path),
+    List = create_erlang_deflate_session(NrProcesses, Level, Bits, Mem),
+
+    io:format("creating deflate session completed ~n"),
+
+    {Time, _} = timer:tc(fun() -> plists:foreach(fun(DeflateRef) -> erlang_process(DeflateRef, Lines, Nr) end, List) end),
+    finish(NrProcesses*Nr*length(Lines), Time),
+    plists:foreach(fun(DeflateRef) -> zlib:close(DeflateRef) end, List).
+
+create_erlang_deflate_session(Nr, Level, Bits, Mem) ->
+    create_erlang_deflate_session(Nr, Level, Bits, Mem, []).
+
+create_erlang_deflate_session(0, _Level, _Bits, _Mem, Acc) ->
+    Acc;
+create_erlang_deflate_session(Nr, Level, Bits, Mem, Acc) ->
+    Z=zlib:open(),
+    ok = zlib:deflateInit(Z, Level, deflated, Bits, Mem, default),
+    create_erlang_deflate_session(Nr-1, Level, Bits, Mem, [Z|Acc]).
+
+create_ezlib_deflate_session(Nr, Options) ->
+    create_ezlib_deflate_session(Nr, Options, []).
+
+create_ezlib_deflate_session(0, _Options, Acc) ->
+    Acc;
+create_ezlib_deflate_session(Nr, Options, Acc) ->
+    {ok, DeflateRef} = ezlib:new(?Z_DEFLATE, Options),
+    create_ezlib_deflate_session(Nr-1, Options, [DeflateRef|Acc]).
 
 read_from_file(Path) ->
     {ok, Device} = file:open(Path, [read]),
