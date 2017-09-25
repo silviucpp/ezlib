35ebd29659f0c1e4b648997279995a67a4a3a742
silviu <silviu.caragea@woowteam.com>
2017-09-25 10:03:10 +0300

    Fix linux build

--- c_src/nif_utils.h
+++ c_src/nif_utils.h
@@ -3,6 +3,8 @@
 
 #include "erl_nif.h"
 
+#include <stdint.h>
+
 ERL_NIF_TERM make_atom(ErlNifEnv* env, const char* name);
 ERL_NIF_TERM make_error(ErlNifEnv* env, const char* error);
 ERL_NIF_TERM make_binary(ErlNifEnv* env, const uint8_t* buff, size_t length);
